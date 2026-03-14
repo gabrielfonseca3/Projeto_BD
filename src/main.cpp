3a7cd2d8d1386ce46e801a7ba51c1ee904cde65e
Gabriel Fonseca <gabriel.alexandre@academico.ufpb.br>
2026-03-14 01:10:02 -0300

    add sol and setup lua

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <pqxx/pqxx>
+#include <sol/sol.hpp>
 
 #include "models/author.hpp"
 #include "models/book.hpp"
@@ -8,4 +10,13 @@
 #include "models/manager.hpp"
 #include "models/sale.hpp"
 
-int main(int, char**) { std::cout << "Hello, from Projeto_BD!\n"; }
+int main(int, char**) {
+  std::cout << "Hello, from Projeto_BD!\n";
+  sol::state lua;
+
+  lua.open_libraries(sol::lib::base, sol::lib::io, sol::lib::math,
+                     sol::lib::table, sol::lib::string);
+  lua.open_libraries(sol::lib::string, sol::lib::io);
+
+  lua.script_file("scripts/interface.lua");
+}
